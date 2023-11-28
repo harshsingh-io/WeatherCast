27077c7aaa43172beb9239b84c7a2ccfab705af0
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-11-28 21:07:50 +0530

    Some UI Fixes

---
+++ app/src/main/java/com/codeenemy/weathercast/utils/Constants.kt
@@ -0,0 +1,31 @@
+package com.codeenemy.weathercast.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object Constants {
+    const val APP_ID: String = "f73531cedb713a0e7ad0f7008e758e97"
+    const val BASE_URL: String = "https://api.openweathermap.org/data/"
+    const val METRIC_UNIT: String = "metric"
+    const val PREFERENCE_NAME = "WeatherAppPreference"
+    const val WEATHER_RESPONSE_DATA = "weather_response_data"
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork?:return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network)?:return false
+
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        }else{
+            val networkInfo = connectivityManager.activeNetworkInfo
+            return networkInfo != null && networkInfo.isConnectedOrConnecting
+        }
+    }
+}
